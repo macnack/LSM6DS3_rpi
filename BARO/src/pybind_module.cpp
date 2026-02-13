94050327ec2b6acfd555c86a2de3acc3d127148c
macnack <krupka.maciej@icloud.com>
2026-02-13 16:16:30 +0100

    feat: added call guard
    Signed-off-by: macnack <krupka.maciej@icloud.com>

--- BARO/src/pybind_module.cpp
+++ BARO/src/pybind_module.cpp
@@ -14,15 +14,21 @@ PYBIND11_MODULE(_bmp390, m) {
   py::class_<bmp390::Bmp390>(m, "Bmp390")
       .def(py::init<std::string, uint8_t, unsigned int>(), py::arg("bus_path") = "/dev/i2c-1",
            py::arg("address") = 0x77, py::arg("retries") = 2)
-      .def("begin", &bmp390::Bmp390::begin)
-      .def("close", &bmp390::Bmp390::close, py::arg("power_down") = true)
-      .def("configure_default", &bmp390::Bmp390::configure_default)
+      .def("begin", &bmp390::Bmp390::begin, py::call_guard<py::gil_scoped_release>())
+      .def("close", &bmp390::Bmp390::close, py::arg("power_down") = true,
+           py::call_guard<py::gil_scoped_release>())
+      .def("configure_default", &bmp390::Bmp390::configure_default,
+           py::call_guard<py::gil_scoped_release>())
       .def("set_sampling", &bmp390::Bmp390::set_sampling, py::arg("pressure_oversampling"),
-           py::arg("temperature_oversampling"), py::arg("odr"), py::arg("iir_filter"))
-      .def("read", &bmp390::Bmp390::read)
-      .def("read_temperature_c", &bmp390::Bmp390::read_temperature_c)
-      .def("read_pressure_pa", &bmp390::Bmp390::read_pressure_pa)
-      .def("read_pressure_hpa", &bmp390::Bmp390::read_pressure_hpa);
+           py::arg("temperature_oversampling"), py::arg("odr"), py::arg("iir_filter"),
+           py::call_guard<py::gil_scoped_release>())
+      .def("read", &bmp390::Bmp390::read, py::call_guard<py::gil_scoped_release>())
+      .def("read_temperature_c", &bmp390::Bmp390::read_temperature_c,
+           py::call_guard<py::gil_scoped_release>())
+      .def("read_pressure_pa", &bmp390::Bmp390::read_pressure_pa,
+           py::call_guard<py::gil_scoped_release>())
+      .def("read_pressure_hpa", &bmp390::Bmp390::read_pressure_hpa,
+           py::call_guard<py::gil_scoped_release>());
 
   m.attr("BMP3_NO_OVERSAMPLING") = py::int_(0x00);
   m.attr("BMP3_OVERSAMPLING_2X") = py::int_(0x01);
